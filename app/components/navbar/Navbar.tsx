b0d3510af58b327bbcb906834fa88d3ec293cf5e
Thushan W A R T <rusiththarindu2@gmail.com>
2024-07-17 00:02:32 +0530

    feat: Type Corrections

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import { User } from "@prisma/client";
-
 import Container from "../Container"
 import Logo from "../navbar/Logo"
 import Search from "../navbar/Search"
 import UserMenu from "./UserMenu"
+import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
-    currentUser?: User | null;
+    currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ({currentUser}) => {
