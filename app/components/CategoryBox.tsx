6325552b1057bc34a1a74206076ba107ba6c9c03
Thushan W A R T <rusiththarindu2@gmail.com>
2024-07-23 22:30:56 +0530

    feat: Categories url

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,4 +1,7 @@
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string";
 
 interface CategoryBoxProps {
   icon: IconType;
@@ -12,8 +15,44 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   icon: Icon,
   selected,
 }) => {
+  //important
+  // The code sets up a function handleClick that manipulates the query parameters in the URL.
+  // It parses the current search parameters, updates the category parameter based on the value of label, and conditionally removes it if it matches the existing value.
+  // Finally, it constructs a new URL with the updated query parameters.
+
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, []);
+
   return (
     <div
+      onClick={handleClick}
       className={`
                     flex 
                     flex-col 
